983e414c58c42f155867929f8c62efdec3488577
Đorđe Relić <dorde.relic@stud.unibas.ch>
2016-02-16 17:52:40 +0100

    Implementation of Viterbi algorithm.

---
+++ DishonestCasino/Viterbi.py
@@ -0,0 +1,34 @@
+from Die import FairDie
+from Die import LoadedDie
+
+class Viterbi(object):
+    """Viterbi algorithm class"""
+
+    def Decode(slef, sequence):
+        n = len(sequence)
+        matrix = []
+        matrix.append([0] * n)
+        matrix.append([0] * n)
+
+        # Filling the matrix with probabilities 
+        matrix[0][0] = FairDie.probabiltyOfRolling(float(sequence[0])) * 0.5
+        matrix[1][0] = LoadedDie.probabiltyOfRolling(float(sequence[0])) * 0.5
+        for i in range(1, n):
+            matrix[0][i] = FairDie.probabiltyOfRolling(float(sequence[i])) * max(matrix[0][i - 1] * (1 - FairDie.switchProability), matrix[1][i - 1] * LoadedDie.switchProability)
+            matrix[1][i] = LoadedDie.probabiltyOfRolling(float(sequence[i])) * max(matrix[0][i - 1] * FairDie.switchProability, matrix[1][i - 1] * (1 - LoadedDie.switchProability))
+
+        #print(matrix[0])
+        #print(matrix[1])
+
+        # Backtracking the solution
+        # Find the highest probability and follow back the path with keeping the probability highest
+        path = ""
+        n -= 1
+        while n > -1:
+            if matrix[0][n] > matrix[1][n]:
+                path += 'F'
+            else:
+                path += 'L'
+
+            n -= 1
+        return path[::-1]
