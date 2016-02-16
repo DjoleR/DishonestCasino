c8c3c5ec033cc267034907b45e92c2b731900e50
Đorđe Relić <dorde.relic@stud.unibas.ch>
2016-02-16 17:54:00 +0100

    Dishonest casino; generating sequence of rolls and decoding the sequence using Viterbi algorithm.

---
+++ DishonestCasino/DishonestCasino.py
@@ -0,0 +1,36 @@
+from Die import LoadedDie
+from Die import FairDie
+from Viterbi import Viterbi
+from random import randint
+
+
+ld = LoadedDie()
+ld.on = True
+fd = FairDie()
+fd.on = False
+
+seqeunce = ""
+dice = ""
+
+for i in range(0,101):
+    if ld.on:
+        seqeunce += str(ld.roll())
+        dice += "L"
+        if ld.switch():
+            ld.on = False
+            fd.on = True
+        continue
+
+    if fd.on:
+        seqeunce += str(fd.roll())
+        dice += "F"
+        if fd.switch():
+            fd.on = False
+            ld.on = True
+        continue
+
+
+print(seqeunce)
+print(dice)
+v = Viterbi()
+print(v.Decode(seqeunce))
